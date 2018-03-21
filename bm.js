d97b09e381a128fb2eedd28e19c5694f6bad4b85
Linas Jakucionis <linasj@gmail.com>
2018-03-21 21:29:43 +1300

    More replacements, convert output from div to textarea

--- bm.js
+++ bm.js
@@ -3,27 +3,26 @@
 function refresh() {
     var textArea = document.getElementById("logEntries");
     var out = document.getElementById("detail");
-    out.innerHTML = "";
 
-
-    var newSource = textArea.value;    
+    out.value = "";
+ 
+    var newSource = textArea.value;   
 
     a = newSource.split(/\n/);
     for (i = 0; i< a.length; i++) {
-
         s = rework(a[i]);
-        out.innerHTML += s;
-        out.innerHTML +=" <br/>";
+        out.value += s;
+        out.value +="\n";
     }
-
 }
 
 function rework(s) {
-    s = s.replace(//, "")
-    s = s.replace(/\d+/, "\\d+");
-    s = s.replace(/\(/, "\\(");
-    s = s.replace(/\)/, "\\)");
-    
+    s = s.replace(/^.*] /, "");
+    s = s.replace(/\./g, "\\.");
+    s = s.replace(/\*/g, "\\*");
+    s = s.replace(/-?\d+/g, "-?\\d+");
+    s = s.replace(/\(/g, "\\(");
+    s = s.replace(/\)/g, "\\)");
     s += "$";
     return s;
 }
